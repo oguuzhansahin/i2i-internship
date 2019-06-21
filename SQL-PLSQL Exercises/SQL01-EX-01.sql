20cd0eb8d1ed601a8d31b75cde0238e46a4790fb
oguuzhansahin <oguuzhansahiin@gmail.com>
2019-06-21 09:08:17 +0300

    Delete SQL01-EX-01.sql

--- SQL-PLSQL Exercises/SQL01-EX-01.sql
+++
@@ -1,5 +0,0 @@
-select * from HR.employees;
-select * from HR.departments;
-
-select employee_id,first_name,department_name 
-from HR.employees JOIN HR.departments on HR.employees.department_id = HR.departments.department_id;
