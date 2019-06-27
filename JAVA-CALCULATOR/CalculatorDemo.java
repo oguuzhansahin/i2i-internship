2ac7d8884d746e8a4779dfce20cf644c44b8b8b4
oguuzhansahin <oguuzhansahiin@gmail.com>
2019-06-27 15:18:21 +0300

    refactored

---
+++ JAVA-CALCULATOR/CalculatorDemo.java
@@ -0,0 +1,26 @@
+package controller;
+
+public class CalculatorDemo {
+	
+	
+	
+		public static double Add (double FirstNumber, double SecondNumber)
+		{
+			return FirstNumber + SecondNumber;
+		}
+		public static double Substract (double FirstNumber, double SecondNumber)
+		{
+			return FirstNumber - SecondNumber;
+		}
+		public static double Multiply (double FirstNumber , double SecondNumber)
+		{
+			return FirstNumber * SecondNumber;
+		}
+		public static double Divide (double FirstNumber, double SecondNumber)
+		{
+			return FirstNumber / SecondNumber;
+		}
+
+	
+
+}
