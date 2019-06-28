920dbaad5df24a2345b6cc6c7a0eac11f803b484
oguuzhansahin <oguuzhansahiin@gmail.com>
2019-06-28 09:26:10 +0300

    Java Test

---
+++ jUnit/src/jUnit/PrimeNumberTest.java
@@ -0,0 +1,26 @@
+package jUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PNTest {
+	PrimeNumber test = new PrimeNumber();
+	
+	@Test
+	public void test() {
+		assertEquals(true,test.isPrime(21));
+		
+	}
+	
+
+}
+	
+	
+	
+
+
